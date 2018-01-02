20e8cdf632d8e9f23844fb25d34c5e5d6f3e49e9
user131656 <user131656@ny218.local>
2018-01-02 11:48:02 -0500

    Initial Commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var etherValueLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
